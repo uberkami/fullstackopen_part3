d3a6dcf869f86594944a3204511a23e2d725eae8
uberkami <uberkami@gmail.com>
2020-01-07 18:49:01 +0100

    heroku upload

---
+++ build/precache-manifest.97362304a467d2e1d67467adc3d6cced.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "703a9613e9af9750aeb9b5157606bf18",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7182ce4ad970ced63ebb",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3372c3d9e4c4e98b8a43",
+    "url": "/static/js/2.fcc5233a.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/static/js/2.fcc5233a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7182ce4ad970ced63ebb",
+    "url": "/static/js/main.5b7a1144.chunk.js"
+  },
+  {
+    "revision": "4230d87a2d3cbabbdf4d",
+    "url": "/static/js/runtime-main.e53b3d9b.js"
+  }
+]);
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+
 // const morgan = require('morgan')
 const cors = require('cors')
 
+app.use(express.static('build'))
 app.use(cors())
 app.use(bodyParser.json())
 
