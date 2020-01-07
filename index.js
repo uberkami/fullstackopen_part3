912fc54e773bcfd02af0232efbab80a2f235379f
uberkami <uberkami@gmail.com>
2020-01-07 16:14:19 +0100

    heroku upload

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const morgan = require('morgan')
+// const morgan = require('morgan')
 const cors = require('cors')
 
 app.use(cors())
 app.use(bodyParser.json())
 
-morgan.token('body', function getBody(req) {
-    return JSON.stringify(req.body)
-})
-app.use(morgan(':method :url :response-time :body'))
+// morgan.token('body', function getBody(req) {
+//     return JSON.stringify(req.body)
+// })
+// app.use(morgan(':method :url :response-time :body'))
 
 let persons = [{
     "persons": [
