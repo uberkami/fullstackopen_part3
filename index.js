54d0ff6b09d45e7db03474801779df4c2853517f
uberkami <uberkami@gmail.com>
2020-01-08 16:17:16 +0100

    uibuild

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-
+const Person = require('./models/person')
 const morgan = require('morgan')
 const cors = require('cors')
 
@@ -14,52 +15,14 @@ morgan.token('body', function getBody(req) {
 })
 app.use(morgan(':method :url :response-time :body'))
 
-const mongoose = require('mongoose')
-// const password = process.env.REACT_APP_DB_PW
-const password = "first_user"
-// DO NOT SAVE YOUR PASSWORD TO GITHUB!!
-const url =`mongodb+srv://first_user:${password}@atlasdb-nz7qo.mongodb.net/people?retryWrites=true&w=majority`
-
-
-mongoose.connect(url, { useNewUrlParser: true })
-
-const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
-    id: Number,
+app.get('/api/persons', (req, res) => {
+    console.log("Person", Person)
+    Person.find({}).then(pers => {
+        console.log('pers', pers)
+        // person list is returned without _id and _v, id is set to _id
+        res.json(pers.map(p => p.toJSON()))
+    })
 })
-
-const Person = mongoose.model('Person', personSchema)
-
-let persons = [{
-    "persons": [
-        {
-            "name": "§§delham",
-            "number": "99923415689",
-            "id": 2
-        },
-        {
-            "name": "Eljjm",
-            "number": "122223415689",
-            "id": 4
-        },
-        {
-            "name": "Elham",
-            "number": "123415689",
-            "id": 5
-        },
-        {
-            "name": "Name 11",
-            "number": "asdasdasd",
-            "id": 6
-        },
-        {
-            "name": "Maleck",
-            "number": "123456852963",
-            "id": 7
-        }
-    ]
-}]
 
 app.get('/info', (req, res) => {
     const time = Date(Date.now())
@@ -70,15 +33,10 @@ app.get('/info', (req, res) => {
 
 app.get('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
-    const person = persons[0].persons.find(pers => {
-        return (pers.id === id)
-    })
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
+    Person.findById(id).then(pers => {
+        response.json(pers.toJSON())
     }
-})
+)})
 
 app.delete('/api/persons/:id', (request, response) => {
     const deleteId = Number(request.params.id)
@@ -93,50 +51,26 @@ app.delete('/api/persons/:id', (request, response) => {
 })
 
 app.post('/api/persons', (request, response) => {
-
-    let genId = Math.floor(Math.random() * 100);
-    let personFound = persons[0].persons.find(pers => pers.id === genId)
-    while (personFound) {
-        genId = Math.floor(Math.random() * 100);
-        personFound = persons[0].persons.find(pers => pers.id === genId)
-    }
     const body = request.body
     if (!body.name || !body.number) {
         return response.status(400).json({
             error: 'Name or number missing'
         })
     }
-    const personNameFound = persons[0].persons.find(pers => pers.name === body.name)
 
-    if (personNameFound) {
-        return response.status(403).json({
-            error: 'Name already exist'
-        })
-    }
     const person = {
         name: body.name,
-        number: body.number,
-        id: genId
+        number: body.number
     }
-
-    persons[0].persons = persons[0].persons.concat(person)
-    response.json(person)
+    person.save().then(response => {
+        response.json(savedNote.toJSON())
+    })
 })
 
 app.get('/', (req, res) => {
     res.send(
         '<h1>Hello World!?</h1><a href="/info">Info</a>')
 })
-
-app.get('/api/persons', (req, res) => {
-    console.log("Person", Person)
-    Person.find({}).then(pers => {
-        console.log('pers', pers)
-        // person list is returned without _id and _v, id is set to _id
-        res.json(pers.map(p => p.toJSON()))
-    })
-})
-
 
 app.get('/persons', (req, res) => {
     res.json(persons)
@@ -147,14 +81,6 @@ const unknownEndpoint = (request, response) => {
 }
 
 app.use(unknownEndpoint)
-
-personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
-    }
-  })
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
