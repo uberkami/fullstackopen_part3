891471445095ce2abf9d31ee26933e45764b45bc
uberkami <uberkami@gmail.com>
2020-01-08 22:20:11 +0100

    uibuild

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.get('/api/persons', (req, res) => {
 
 app.get('/info', (req, res) => {
     const time = Date(Date.now())
-    const count = 0
+    let count = 0
     Person.find({}).then(pers => {count += 1})
     console.log('count', count, typeof(count))
     res.send(`<div>Phonebook has info for ${count} people</div>
